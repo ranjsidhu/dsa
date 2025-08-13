441304aab1b5381b1dfd1e7cb379b47457252136
Ranj Sidhu <ranjsidhudev@gmail.com>
2025-08-13 20:49:50 +0100

    feat(dll): prepend

--- dll/dll-intro.py
+++ dll/dll-intro.py
@@ -45,12 +45,23 @@ class DoublyLinkedList:
         
         self.length -= 1
         return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+        
+        self.length += 1
+        return new_node
             
 
 
 
 dll = DoublyLinkedList(1)
-dll.append(2)
-dll.append(3)
-dll.pop()
+dll.prepend(0)
 dll.print_list()
