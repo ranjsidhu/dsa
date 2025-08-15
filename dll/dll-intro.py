4d90a6f65863a894584633948207e02c02021ef3
Ranj Sidhu <ranjsidhudev@gmail.com>
2025-08-15 14:14:02 +0100

    feat(dll): remove

--- dll/dll-intro.py
+++ dll/dll-intro.py
@@ -132,5 +132,26 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
 
-dll = DoublyLinkedList(1)
+        if index == 0:
+            return self.pop_first()
+
+        if index == self.length - 1:
+            return self.pop()
+
+        temp = self.get_by_binary_search(index)
+
+        temp.prev.next = temp.next
+        temp.next.prev = temp.prev
+
+        temp.next = None
+        temp.prev = None
+
+        self.length -= 1
+        return temp
+
+
+dll = DoublyLinkedList(11)
