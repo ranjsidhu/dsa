22f7b4c919cfd491e9e427745e2ea71fe36f2280
Ranj Sidhu <ranjsidhudev@gmail.com>
2025-08-15 15:50:42 +0100

    feat(dll): is palindrome

--- dll/dll_is_palindrome.py
+++ dll/dll_is_palindrome.py
@@ -1,1 +1,28 @@
-from dll_intro import DoublyLinkedList, Node
+from dll_intro import DoublyLinkedList
+
+dll_list = DoublyLinkedList(1)
+dll_list.append(2)
+dll_list.append(3)
+dll_list.append(2)
+dll_list.append(1)
+dll_list.print_list()
+
+
+def is_palindrome(dll):
+    if dll.length == 0 or dll.length == 1:
+        return True
+
+    forward = dll.head
+    backward = dll.tail
+    iterations = dll.length // 2
+
+    for _ in range(1, iterations + 1):
+        if forward.value != backward.value:
+            return False
+        forward = forward.next
+        backward = backward.prev
+
+    return True
+
+
+print(is_palindrome(dll_list))
