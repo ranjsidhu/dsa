0896979c8098fba3a70b98d01a6f5663b5a7b1b2
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2025-08-15 12:40:22 +0100

    feat(dll): get and get by binary search

--- dll/dll-intro.py
+++ dll/dll-intro.py
@@ -4,19 +4,22 @@ class Node:
         self.next = None
         self.prev = None
 
+
 class DoublyLinkedList:
     def __init__(self, value):
         new_node = Node(value)
-        self.head= new_node
+        self.head = new_node
         self.tail = new_node
         self.length = 1
-    
+
     def print_list(self):
         temp = self.head
+        print("------- Printing list starts -------")
         while temp is not None:
             print(temp.value)
             temp = temp.next
-        
+        print("------- Printing list ends -------")
+
     def append(self, value):
         new_node = Node(value)
 
@@ -27,7 +30,7 @@ class DoublyLinkedList:
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
-        
+
         self.length += 1
         return True
 
@@ -42,7 +45,7 @@ class DoublyLinkedList:
             self.tail = self.tail.prev
             self.tail.next = None
             temp.prev = None
-        
+
         self.length -= 1
         return temp
 
@@ -55,10 +58,10 @@ class DoublyLinkedList:
             self.head.prev = new_node
             new_node.next = self.head
             self.head = new_node
-        
+
         self.length += 1
         return True
-    
+
     def pop_first(self):
         if self.length == 0:
             return None
@@ -70,17 +73,36 @@ class DoublyLinkedList:
             self.head = self.head.next
             self.head.prev = None
             temp.next = None
-        
+
         self.length -= 1
         return temp
-            
+
+    def get(self, index):
+        if self.length == 0 or index > self.length - 1:
+            return None
+        temp = self.head
+        if index == 0 and temp is not None:
+            return temp.value
+        count = 0
+        while count != index:
+            temp = temp.next
+            count += 1
+
+        return temp
+
+    def get_by_binary_search(self, index):
+        if self.length == 0 or index > self.length - 1:
+            return None
+
+        if index < self.length / 2:
+            return self.get(index)
+        else:
+            temp = self.tail
+            steps = self.length - 1 - index
+            for _ in range(steps):
+                temp = temp.prev
+
+            return temp
 
 
-
-dll = DoublyLinkedList(1)
-dll.pop_first()
-dll.print_list()
-dll.append(2)
-dll.append(3)
-dll.pop_first()
-dll.print_list()
+dll = DoublyLinkedList(11)
