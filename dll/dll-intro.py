b80a1c2178005f6b014c15e542db9e1746c359a9
Ranj Sidhu <ranjsidhudev@gmail.com>
2025-08-15 13:55:20 +0100

    feat(dll): insert

--- dll/dll-intro.py
+++ dll/dll-intro.py
@@ -112,5 +112,25 @@ class DoublyLinkedList:
 
         return False
 
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
 
-dll = DoublyLinkedList(11)
+        new_node = Node(value)
+        before = self.get_by_binary_search(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
+        return True
+
+
+dll = DoublyLinkedList(1)
