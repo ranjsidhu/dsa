9212e1119b2492fc9afd3f1d25e6f6bd6d182540
Ranjeet Sidhu <ranjeetsidhu7433@gmail.com>
2025-08-17 17:21:04 +0100

    feat(bst): contains

--- binary_search_trees/bst_intro.py
+++ binary_search_trees/bst_intro.py
@@ -31,3 +31,46 @@ class BinarySearchTree:
                     temp.right = new_node
                     return True
                 temp = temp.right
+
+    def contains(self, value):
+        current = self.root
+        while current is not None:
+            print(f"in while loop and current is: {current.value}")
+            if current.value == value:
+                return True
+            elif value < current.value:
+                current = current.left
+            else:
+                current = current.right
+
+        return False
+
+    def print_tree(self, node=None, prefix="", is_left=True):
+        if node is None:
+            if prefix == "":
+                node = self.root
+            else:
+                return
+
+        if node.right:
+            new_prefix = prefix + ("│   " if is_left else "    ")
+            self.print_tree(node.right, new_prefix, False)
+
+        print(prefix + ("└── " if is_left else "┌── ") + str(node.value))
+
+        if node.left:
+            new_prefix = prefix + ("    " if is_left else "│   ")
+            self.print_tree(node.left, new_prefix, True)
+
+
+bst = BinarySearchTree()
+bst.insert(47)
+bst.insert(21)
+bst.insert(76)
+bst.insert(18)
+bst.insert(27)
+bst.insert(52)
+bst.insert(82)
+bst.print_tree()
+print("----------- Result -----------")
+print(bst.contains(17))
