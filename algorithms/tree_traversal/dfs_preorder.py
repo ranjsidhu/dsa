c2b19f613e0bffea9d11110f0b6034851e79120a
Ranjeet Sidhu <ranjsidhudev@gmail.com>
2025-09-09 20:42:35 +0100

    feat(dfs): dfs preorder

--- algorithms/tree_traversal/dfs_preorder.py
+++ algorithms/tree_traversal/dfs_preorder.py
@@ -1,2 +1,16 @@
 # This is the standalone code
 # see data_structures/binary_search_trees/bst_intro.py for the implementation
+
+
+def dfs_pre_order(self):
+    results = []
+
+    def traverse(current_node):
+        results.append(current_node.value)
+        if current_node.left is not None:
+            traverse(current_node.left)
+        if current_node.right is not None:
+            traverse(current_node.right)
+
+    traverse(self.root)
+    return results
--- data_structures/binary_search_trees/bst_intro.py
+++ data_structures/binary_search_trees/bst_intro.py
@@ -89,14 +89,23 @@ class BinarySearchTree:
 
         return results
 
-    def dfs_preorder(self):
-        current_node = self.root
+    def dfs_pre_order(self):
         results = []
 
-        return None
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
 
 
 bst = BinarySearchTree()
 bst.initialise_tree()
-print("----------- Result -----------")
+print("----------- Result - BFS -----------")
 print(bst.bfs())
+print("----------- Result - DFS Preorder -----------")
+print(bst.dfs_pre_order())
