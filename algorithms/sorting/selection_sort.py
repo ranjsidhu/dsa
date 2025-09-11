8e66bb70728f575c9a6bbd5215c52d7f77fb8228
Ranjeet Sidhu <ranjsidhudev@gmail.com>
2025-09-11 20:33:37 +0100

    feat(sort): selection sort

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,17 @@
+list = [4, 2, 6, 5, 1, 3]
+
+
+def selection_sort(arr):
+    for i in range(len(arr) - 1):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+print(selection_sort(list))
